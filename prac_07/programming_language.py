f84f77eed4b661a0cc3b779780890c4d99b0efd6
Ramiz <ramizkarim05@gmail.com>
2024-11-08 10:05:35 +1000

    Add another language to the file

---
+++ prac_07/programming_language.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Programming Language class with tests.
+"""
+
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic=False):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+    def __repr__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}")
+
+    def __str__(self):
+        """Return a more user-friendly string representation of a ProgrammingLanguage."""
+        return (f"{self.name} ({self.typing} Typing), Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, Year={self.year}")
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+
